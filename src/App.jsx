50d0fc5e524cb63aa8e0a28ebae2e4c99376f7f4
Ashish Pandey <126936221+Ashish-Pandey0927@users.noreply.github.com>
2024-11-17 22:19:11 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Hero from './Components/Hero'
 import Navbar from './Components/Navbar'
 import Section2 from './Components/Section2'
 import Section3 from './Components/Section3'
-import Section4 from './Components/section4'
+import Section4 from './Components/Section4'
 
 const App = () => {
   return (
